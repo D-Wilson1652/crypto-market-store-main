040512a90713ae6da0973f2a0a888d9dec059fe6
Dylan Wilson <98748077+D-Wilson1652@users.noreply.github.com>
2024-12-05 11:30:25 +0000

    Update home-featured-products-carousel.tsx

--- src/components/home/featured-products/home-featured-products-carousel.tsx
+++ src/components/home/featured-products/home-featured-products-carousel.tsx
@@ -1,34 +1,18 @@
 
 // Config Import
-import {Honeycomb, ResponsiveHoneycomb} from "react-honeycomb-v2";
+import {ResponsiveHoneycomb} from "react-honeycomb-v2";
 import CategoryCard from "@/components/sell-with-us/category-card.tsx";
 import {categoryCardsData} from "@/components/sell-with-us/data.ts";
 
 // ! Dummy Data
-const buySellData = [
-    {
-        image: "/images/sell-with-us/category-1.png",
-        name: "buyer",
-        link: null,
-    },
-    {
-        image: "/images/sell-with-us/category-1.png",
-        name: "Seller",
-        link: null,
-    },
-    {
-        image: "/images/sell-with-us/category-1.png",
-        name: "Auction only",
-        link: null,
-    },
-]
+
 const HomeFeaturedProductsCarousel = () => {
     const items = categoryCardsData;
     const size = 150;
 
     return(<div className="App">
         <ResponsiveHoneycomb
-            columns={1}
+            defaultWidth={1}
             size={size}
             items={items}
             className={"gap-4"}
