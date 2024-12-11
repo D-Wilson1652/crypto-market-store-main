4f4bf7792f5b71116f6f4699bf917bc30220ab2b
Dylan Wilson <98748077+D-Wilson1652@users.noreply.github.com>
2024-12-11 13:09:51 +0000

    Update pricing-block.tsx

--- src/components/pricing/pricing-block.tsx
+++ src/components/pricing/pricing-block.tsx
@@ -10,9 +10,14 @@ import { toast } from "sonner";
 
 // Data import
 import { pricingDataMonthly, pricingDataAnnualy } from "@/data/pricing-card";
+import Text from "@/components/ui/typography/text.tsx";
+import Button from "@/components/ui/button.tsx";
+import clsx from "clsx";
 
 export default function PricingBlock() {
-    const [state, setState] = useState("annually");
+    /*
+    *
+    * */const [state, setState] = useState("annually");
 
     const [
         createSubscriptionSession,
@@ -34,12 +39,9 @@ export default function PricingBlock() {
         },
         [createSubscriptionSession]
     );
-
-    return (
-        <div>
-            <PricingHero state={state} onClick={(value) => setState(value)} />
-
-            <div className="container-fluid grid w-full !max-w-[1296px] gap-4 py-12 sm:grid-cols-2 lg:grid-cols-3 lg:gap-6 lg:py-20 3xl:px-0 3xl:py-24">
+/*
+<PricingHero state={state} onClick={(value) => setState(value)} />
+* <div className="container-fluid grid w-full !max-w-[1296px] gap-4 py-12 sm:grid-cols-2 lg:grid-cols-3 lg:gap-6 lg:py-20 3xl:px-0 3xl:py-24">
                 {state === "monthly"
                     ? Object.values(pricingDataMonthly).map((pricingData) => (
                           <PricingCard
@@ -60,6 +62,19 @@ export default function PricingBlock() {
                           />
                       ))}
             </div>
+
+* */
+    return (
+        <div className="mt-8 text-center lg:mt-20 2xl:mt-24">
+            <div className="mx-auto max-w-4xl">
+                <Text tag="h1" className="text-[28px] font-bold leading-10">
+                    Coming Soon
+                </Text>
+                <Text className="mt-2 !text-sm 2xl:mt-4">
+                    This Item Is currently Under Development. Please Try again later
+                </Text>
+            </div>
         </div>
+
     );
 }
